83ac0c1563661d3d0a63aa3049845cc733bf47d1
NARENDRA G S <narenskills@gmail.com>
2026-01-13 23:09:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// Donate form
+document.getElementById("donateForm")?.addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Donation submitted successfully!");
+});
+
+// Request form
+document.getElementById("requestForm")?.addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Food request submitted successfully!");
+});
+
+// Contact form
+document.getElementById("contactForm")?.addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Message sent successfully!");
+});
