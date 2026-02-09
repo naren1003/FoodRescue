7f529b97e01ac4342fbdaed6fe8caaecd90604d1
NARENDRA G S <narenskills@gmail.com>
2026-02-09 16:10:28 +0530

    update

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-// Donate form
-document.getElementById("donateForm")?.addEventListener("submit", function(e){
-    e.preventDefault();
-    alert("Donation submitted successfully!");
-});
 
-// Request form
-document.getElementById("requestForm")?.addEventListener("submit", function(e){
-    e.preventDefault();
-    alert("Food request submitted successfully!");
-});
-
-// Contact form
-document.getElementById("contactForm")?.addEventListener("submit", function(e){
-    e.preventDefault();
-    alert("Message sent successfully!");
-});
