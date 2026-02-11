5561cd19fdece20f0413c2aaf08f0f92c53b3844
NARENDRA G S <narenskills@gmail.com>
2026-02-11 11:45:03 +0530

    update final

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function SubmitFirst(){
+    alert("Submitted");
+}
 
+function addName(){
+    document.getElementById("extra").innerHTML=`
+    <div class="sign-in-input">
+        <p style="text-align: left; margin-bottom: 10px;"><b>Restaurant/Organization Name</b></p>
+        <input type="text" name="name" placeholder="Your Restaurant Name" required>
+    </div>`
+}
